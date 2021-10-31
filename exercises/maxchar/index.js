a26526652c1286b40a9641ed620e63594873c882
Ugochukwu Ezenwankwo <ugoezenwankwo@gmail.com>
2021-10-31 16:48:10 +0100

    MAx Character in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //  My Solution
+  // const res = {};
+  // let highest = 0;
+  // let prop;
+  // str.split('').forEach((x) => {
+  //   if (!res[x]) {
+  //     res[x] = 1;
+  //   } else {
+  //     res[x] = res[x] + 1;
+  //   }
+  // })
+  // Object.keys(res).forEach(x => {
+  //   if (res[x] > highest) {
+  //     highest = res[x];
+  //     prop = x;
+  //   }
+  // })
+  // return prop;
+
+  // Solution 1
+  const charMap = {};
+  let max = 0;
+  let maxC = '';
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxC = char;
+    }
+  }
+  return maxC;
+
+}
 
 module.exports = maxChar;
