be9efe094c09d5ef352585371b691be88d483a5a
Ugochukwu Ezenwankwo <ugoezenwankwo@gmail.com>
2021-11-19 19:48:08 +0100

    Chunk challenge

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //  My Solution
+  // let arr = [];
+  // for (var i = array.length; i > 0; i = array.length){
+  //   const spliced = array.splice(0, size);
+  //   arr.push(spliced);
+  // }
+  // return arr;
+
+  // Solution 1
+  // const chunked = [];
+  // for (let element of array) {
+  //   const last = chunked[chunked.length - 1];
+  //   if (!last || last.length === size) {
+  //     chunked.push([element]);
+  //   } else {
+  //     last.push(element);
+  //   }
+  // }
+  // return chunked;
+
+  // Solutiom 2
+  const chunked = [];
+  let index = 0;
+  while(index < array.length){
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
