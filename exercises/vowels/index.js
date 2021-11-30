413a08aa8baea2827f027f78ab36ae1938b14e83
Ugochukwu Ezenwankwo <ugoezenwankwo@gmail.com>
2021-11-30 13:16:58 +0100

    Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,31 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //  My Solution
+  // str = str.toLowerCase();
+  // const vowels = ['a', 'e', 'i', 'o', 'u'];
+  // let noOfVowels = 0;
+  // for (let i = 0; i < str.length; i++) {
+  //   if (vowels.includes(str[i])) {
+  //     noOfVowels++;
+  //   }
+  // }
+  // return noOfVowels;
+
+  // Solution 1
+  // let count = 0;
+  // const checker = ['a', 'e', 'i', 'o', 'u'];
+  // for (let char of str.toLowerCase()) {
+  //   if (checker.includes(char)) {
+  //     count++;
+  //   }
+  // }
+  // return count;
+
+  // Solution 2
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
