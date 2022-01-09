5720cadb63f32e0cb68d99a062caf91b1f8d6789
Ugochukwu Ezenwankwo <ugoezenwankwo@gmail.com>
2022-01-09 11:25:14 +0100

    Fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,55 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+  //  My Solution
+  // let result = [];
+  // for (let i = 0; i <= n; i++) {
+  //   if (i === 0 || i === 1) {
+  //     result.push(i);
+  //   } else {
+  //     result.push(result[i-2] + result[i-1])
+  //   }
+  // }
+  // return result[n];
+
+  //  Solution 1
+  // const result = [0, 1];
+  // for (let i = 2; i <= n; i++) {
+  //   const a = result[i-1];
+  //   const b = result[i-2];
+  //   result.push(a+b);
+  // }
+  // return result[n];
+
+  //  Solution 2 (Recursion)
+  // if (n < 2) {
+  //   return n;
+  // }
+  // return fib(n-1) + fib(n-2);
+// }
+
+//  Solution 3 (Recursion and memoization)
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
